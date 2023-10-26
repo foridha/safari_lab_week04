1e9f13129ade250153554bd037048aa47b3b8f5c
Foridha Ahmed <foridha@Foridhas-MacBook-Air.local>
2023-10-26 15:38:08 +0100

    first commit

---
+++ safari.sql
@@ -0,0 +1,37 @@
+
+
+
+
+CREATE TABLE (
+	id SERIAL PRIMARY KEY
+	name "Tony",
+	"type": "Tiger",
+	"age": 59,
+	"enclosure_id": 1
+);
+
+// enclosure
+
+{
+	"id": 1,
+	"name": "big cat field",
+	"capacity": 20,
+	"closedForMaintenance": false
+}
+
+// staff
+
+{
+	"id": 1,
+	"name": "Captain Rik",
+	"employeeNumber": 12345,
+}
+
+// assignment
+
+{
+	"id": 1,
+	"employeeId": 1,
+	"enclosureId": 1,
+	"day": "Tuesday"
+}
